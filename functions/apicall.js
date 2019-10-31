a0996f0372c89060bc6de8c8ae3cc657a9414fa0
Zweihänder <zweidev@zweihander.me>
2019-10-30 20:40:09 -0400

    Temp switch for Netlify testing

--- functions/apicall.js
+++ functions/apicall.js
@@ -1,6 +1,6 @@
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
-const apiKey = process.env.MAP_GOOGLEAPIS_KEY;
+// const apiKey = process.env.MAP_GOOGLEAPIS_KEY;
 
 exports.handler = function(event, context, callback) {
 	// your server-side functionality
